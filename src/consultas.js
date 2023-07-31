ca8000e4f2487f62ceba22ee9cbc321d9482951c
Sergio Zegarra <s.zegarra@hotmail.com>
2023-07-30 22:54:55 -0500

    agregando para que funciones con las pollerias

--- src/consultas.js
+++ src/consultas.js
@@ -38,12 +38,47 @@ async function obtenerCabecerasCDP(RUC) {
 }
 
 
-async function guardarRespuesta(DATA){    
+async function guardarRespuesta(DATA,TABLA=''){    
     try {
         const connection = await pool.getConnection();
-        await connection.query('insert into tbl2_validez_sunat (idx, ruc_, tip_ope, estado,anulado,fecha_consulta,detalles) values (?,?,?,?,?,?,?)',DATA);
+        await connection.query(`insert into tbl2_validez_sunat${TABLA} (idx, ruc_, tip_ope, estado,anulado,fecha_consulta,detalles) values (?,?,?,?,?,?,?)`,DATA);
         console.log("carchivo registrado ", DATA[1],' ',DATA[0]);
         connection.release();
+    } catch (error) {
+        console.error('Error al realizar la consulta:', error);
+    }
+}
+
+
+
+async function obtenerCabecerasPolloCDP(RUC) {
+    try {
+        const connection = await pool.getConnection();
+        const query = ` 
+                        SELECT 
+                        CDP.idx, CDP.ruc_, CDP.tip_ope, CDP.SERIE, CDP.NUMERO, CDP.total as tot_vta, DATE_FORMAT(fec_ope, '%d/%m/%Y') as fec_ope  
+                        FROM (
+                                    SELECT 
+                                        A.idx, A.ruc_, A.tip_ope,  A.SERIE, A.NUMERO, A.fec_ope , A.total  
+                                                FROM TTBL_CAB_VILLA A
+                                                LEFT JOIN tbl2_validez_sunat_POLLO C ON A.idx = C.idx
+                                                WHERE 
+                                                    C.idx IS NULL AND A.ruc_='${RUC}' AND A.tip_ope in ('01','03') AND  YEAR(A.fec_ope)>=2023
+                                UNION                               
+                    
+                                    SELECT 
+                                        B.idx, B.ruc_, B.tip_ope, B.SERIE, B.NUMERO, B.fec_ope, B.total  
+                                                FROM TTBL_CAB_VILLA_NC B
+                                                LEFT JOIN tbl2_validez_sunat_POLLO C ON B.idx = C.idx
+                                                WHERE 
+                                                    C.idx IS NULL AND B.ruc_='${RUC}' AND B.tip_ope in ('07','08') AND  YEAR(B.fec_ope)>=2023
+                            ) AS CDP
+                                        LIMIT 150
+                         
+                    `;        
+        const [rows, fields] = await connection.query(query);        
+        connection.release();
+        return rows;        
     } catch (error) {
         console.error('Error al realizar la consulta:', error);
     }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 require('dotenv').config();
 const RUC_LOCAL = process.env.RUC_LOCAL
 const { TOKEN, VERIFICAR } = require('./token');
-const { obtenerCabecerasCDP, guardarRespuesta } = require('./consultas.js');
+const { obtenerCabecerasCDP,obtenerCabecerasPolloCDP, guardarRespuesta } = require('./consultas.js');
 
 
 let procesar = async () => {
     try {
-        let to = await TOKEN();
-        //console.log(to.access_token);
+        let to = await TOKEN();        
         let rows = await obtenerCabecerasCDP(RUC_LOCAL);
-
         console.log("\nTotal Registros encontrados: ",rows.length);
-
         await rows.map(async (row, index) => {
             let DATOS = {
                 'numRuc': row.ruc_,
@@ -21,35 +18,80 @@ let procesar = async () => {
                 'fechaEmision': row.fec_ope,
                 'monto': row.tot_vta
             };
-
             setTimeout(async () => {
                 let RESP = await VERIFICAR(RUC_LOCAL, to.access_token, DATOS);
                 if (RESP.success == true) {
                     const fecha = new Date(); // Obtén la fecha actual de JavaScript            
-                    const now = fecha.toISOString().slice(0, 19).replace('T', ' ');
-                    //console.log(now);
+                    const now = fecha.toISOString().slice(0, 19).replace('T', ' ');                    
                     if (RESP.data.estadoCp != null)
                         await guardarRespuesta([row.idx, row.ruc_, row.tip_ope, RESP.data.estadoCp, 0, now, null]);
                 }
-
             }, 50 * index)
         });
 
-
         setTimeout(async () => {
             console.log(`\n\nVolviendo a buscar ${Date.now()} \n`);
-            await procesar();
+            await ejecutar();
         }, 30 * 1000);
 
     } catch (error) {
         console.log("Error de Verificacion: ", error);
         setTimeout(async () => {
             console.log("\n\nVolviendo a buscar desde el error \n");
-            await procesar();
+            await ejecutar();
         }, 10 * 1000);
     }
-
-
 }
 
-procesar();
+//aqui biene el proceso del pollo
+let procesar_pollo = async () => {
+    try {
+        let to = await TOKEN();        
+        let rows = await obtenerCabecerasPolloCDP(RUC_LOCAL);
+        console.log("\nTotal Registros pollo encontrados: ",rows.length);
+        await rows.map(async (row, index) => {
+            let DATOS = {
+                'numRuc': row.ruc_,
+                'codComp': row.tip_ope,
+                'numeroSerie': row.SERIE,
+                'numero': row.NUMERO,
+                'fechaEmision': row.fec_ope,
+                'monto': row.tot_vta
+            };
+            setTimeout(async () => {
+                let RESP = await VERIFICAR(RUC_LOCAL, to.access_token, DATOS);
+                if (RESP.success == true) {
+                    const fecha = new Date(); // Obtén la fecha actual de JavaScript            
+                    const now = fecha.toISOString().slice(0, 19).replace('T', ' ');                    
+                    if (RESP.data.estadoCp != null)
+                        await guardarRespuesta([row.idx, row.ruc_, row.tip_ope, RESP.data.estadoCp, 0, now, null],'_POLLO');
+                }
+            }, 50 * index)
+        });
+
+        setTimeout(async () => {
+            console.log(`\n\nVolviendo a buscar ${Date.now()} \n`);
+            await ejecutar();
+        }, 30 * 1000);
+
+    } catch (error) {
+        console.log("Error de Verificacion: ", error);
+        setTimeout(async () => {
+            console.log("\n\nVolviendo a buscar desde el error \n");
+            await ejecutar();
+        }, 10 * 1000);
+    }
+}
+
+
+async function ejecutar(){
+    if(process.env.TIPO=='POLLO'){
+        console.log("Procendo Pollo")
+        await procesar_pollo()
+    }else{
+        console.log("Procendo Normal")
+        await procesar()
+    }
+}
+
+ejecutar();
